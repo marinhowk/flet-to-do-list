1af51a718e0c67d60bc20b25efbf9f601bab64b9
João Marinho <contato.marinhowork@gmail.com>
2026-02-18 00:09:53 -0300

    Adicionando atualizações

--- app.py
+++ app.py
@@ -7,6 +7,39 @@ def main(page: ft.Page):
     FG = "#3450a1"
     PINK = "#eb06ff"
 
+    categories_card = ft.Row(
+        scroll="auto"
+    )
+
+    categories = ["Businness", "Family", "Friends"]
+
+    for i, category in enumerate(categories):
+        categories_card.controls.append(
+            ft.Container(
+                bgcolor= BG, 
+                border_radius=20,
+                height=110, 
+                width=170,
+                padding=15,
+                content= ft.Column(
+                    controls=[
+                        ft.Text('40 tasks'),
+                        ft.Text(category),
+                        ft.Container(
+                            bgcolor= "white12",
+                            width=160,
+                            height=5,
+                            border_radius=20,
+                            padding=ft.padding.only(right=i*30),
+                            content=ft.Container(
+                                bgcolor=PINK,
+                            )
+                        )
+                        ]  
+                )
+            )
+        )
+
     first_page_contents = ft.Container(
         content= ft.Column(
             controls= [
@@ -24,8 +57,8 @@ def main(page: ft.Page):
                 ft.Container(height=20),
                 ft.Text(value="What's up, John! "),
                 ft.Text(value="CATEGORIES"),
-                ft.Container( padding=ft.padding.only(top=20, bottom=20,)
-                             )
+                ft.Container( padding=ft.padding.only(top=20, bottom=20,),
+                             content=categories_card)
             ]
         )
     )
