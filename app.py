6afac6ffb9dd7dd021efb58b450170ab617324cd
João Marinho <contato.marinhowork@gmail.com>
2026-02-18 15:59:53 -0300

    Atualizando progresso de desenvolvimento

--- app.py
+++ app.py
@@ -7,6 +7,28 @@ def main(page: ft.Page):
     FG = "#3450a1"
     PINK = "#eb06ff"
 
+
+    create_task_view = ft.Container(
+        content= ft.Container(on_click=lambda _: page.go("/"),height=40, width=40, content=ft.Text('x'))
+    )
+
+    tasks = ft.Column(
+        height=400,
+        scroll="auto",
+        #controls= [
+            #ft.Container(height=50, width=300, bgcolor="red"),
+            #ft.Container(height=50, width=300, bgcolor="red"),
+            #ft.Container(height=50, width=300, bgcolor="red"),
+            #ft.Container(height=50, width=300, bgcolor="red"),
+        #    ]
+
+    )
+
+    for i in range(10):
+        tasks.controls.append(
+            ft.Container(height=50, width=400, bgcolor=BG, border_radius=20),
+        )
+
     categories_card = ft.Row(
         scroll="auto"
     )
@@ -58,9 +80,22 @@ def main(page: ft.Page):
                 ft.Text(value="What's up, John! "),
                 ft.Text(value="CATEGORIES"),
                 ft.Container( padding=ft.padding.only(top=20, bottom=20,),
-                             content=categories_card)
-            ]
-        )
+                             content=categories_card),
+                ft.Container(height=20),
+                ft.Text("TODAY'S TASKS"),
+                ft.Stack(
+                    controls=[
+                        tasks,
+                        ft.FloatingActionButton(
+                            icon=ft.Icons.ADD, 
+                            bottom=2, 
+                            right=20, 
+                            on_click=lambda _: page.go("/create_task"),
+                            ),
+                    ],
+                ),
+            ],
+        ),
     )
 
     page_1 = ft.Container()
@@ -72,19 +107,10 @@ def main(page: ft.Page):
                 height=850,
                 bgcolor=FG,
                 border_radius=35,
-                padding= ft.padding.only(
-                    top=50,
-                    left=20,
-                    right=20,
-                    bottom=5
-                ),
-                content= ft.Column(
-                    controls=[
-                        first_page_contents
-                    ]
-                )
+                padding= ft.padding.only(top=50, left=20, right=20 ,bottom=5),
+                content= ft.Column(controls=[first_page_contents ]),
             )
-        ]
+        ],
     )
 
     container = ft.Container (
@@ -95,11 +121,29 @@ def main(page: ft.Page):
         content= ft.Stack (
             controls= [
                 page_1,
-                page_2
+                page_2,
             ]
-        )
+        ),
     )
 
+    pages = {
+        "/": ft.View (
+            "/",
+            [container],
+        ),
+        "/create_task": ft.View(
+            "/create_task",
+            [create_task_view],
+        ),
+    }
+
+    def route_change(e):
+        page.views.clear()
+        page.views.append(pages[page.route])
+        page.update()
+
     page.add(container)
+    page.on_route_change = route_change
+    page.go("page.route")
 
 ft.run(main)
